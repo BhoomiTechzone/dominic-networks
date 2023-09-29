56a9e63f9f5bc35f255641f989a7da2750e669d2
Abhishek Yadav <92436824+Bhi2710@users.noreply.github.com>
2023-09-29 15:43:33 +0530

    29Setp

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,67 +1,90 @@
 import * as React from 'react';
-import { useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import IconButton from '@mui/material/IconButton';
+// import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
-import PlayArrowIcon from '@mui/icons-material/PlayArrow';
-import SkipNextIcon from '@mui/icons-material/SkipNext';
+import { MenuItem, Select } from '@mui/material';
+
 
 
 const Footer = () => {
-    const theme = useTheme();
+
     return (
         <>
-            <Card sx={{ display: 'flex',justifyContent:"space-between" , backgroundColor:"black"}}>
+            <Card sx={{ display: 'flex', justifyContent: "space-between", backgroundColor: "black", flexWrap: "wrap" }}>
                 <CardMedia
                     component="img"
-                    sx={{ width: 151 }}
+                    sx={{ width: 251 }}
                     image="/static/images/cards/live-from-space.jpg"
                     alt="Live from space album cover"
                 />
                 <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                    <CardContent sx={{ flex: '1 0 auto' }}>
-                        <Typography color="white" component="div" variant="h5">
-                           | Live From Space
+                    <CardContent sx={{ flex: '1'}}>
+                        <Box sx={{ display: "flex", alignItems: "center" }}>
+                            <Box sx={{ backgroundColor: "white", width: "2px", height: "30px" }} ></Box>
+                            <Typography color="white" component="div" variant="h5" m="8px">
+                              Quick Links
+                            </Typography>
+                        </Box>
+                        <Typography sx={{borderBottom: "1px dashed white"}} variant="subtitle1" color="white" component="div">
+                          Home
                         </Typography>
-                        <Typography variant="subtitle1" color="white" component="div">
-                            Mac Miller
+                        <Typography sx={{borderBottom: "1px dashed white"}} variant="subtitle1" color="white" component="div">
+                          About Us
                         </Typography>
-                    </CardContent>   
+                        <Typography sx={{borderBottom: "1px dashed white"}} variant="subtitle1" color="white" component="div">
+                          <Select
+                            label="For Indvisible"
+                          >
+                          <MenuItem >OTT</MenuItem>
+                          </Select>
+                        </Typography>
+                        <Typography sx={{borderBottom: "1px dashed white"}} variant="subtitle1" color="white" component="div">
+                          Contact Us
+                        </Typography>
+                    </CardContent>
                 </Box>
 
                 <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                    <CardContent sx={{ flex: '1 0 auto' }}>
-                        <Typography color="white" component="div" variant="h5">
-                           | Live From Space
+                    <CardContent sx={{ flex: '1', width: "80%" }}>
+                        <Box sx={{ display: "flex", alignItems: "center" }}>
+                            <Box sx={{ backgroundColor: "white", width: "2px", height: "30px" }} ></Box>
+                            <Typography color="white" component="div" variant="h5" m="8px">
+                                Helpful Links
+                            </Typography>
+                        </Box>
+                        <Typography variant="subtitle1" color="white" component="div">
+                            <b>Dominic Networks</b> Pvt. Ltd.
                         </Typography>
                         <Typography variant="subtitle1" color="white" component="div">
-                            Mac Miller
+                            <b>Head Office : </b> A-36, Sector-63 Noida Uttar Pradesh
                         </Typography>
-                    </CardContent>   
+                        <Typography variant="subtitle1" color="white" component="div">
+                            <b>Call Us: +91-1111111111</b>
+                        </Typography>
+                    </CardContent>
                 </Box>
 
                 <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                    <CardContent sx={{ flex: '1' }}>
-                        <Typography color="white" component="div" variant="h5">
-                           | Live From Space
+                    <CardContent sx={{ flex: '1', width: "80%" }}>
+                        <Box sx={{ display: "flex", alignItems: "center" }}>
+                            <Box sx={{ backgroundColor: "white", width: "2px", height: "30px" }} ></Box>
+                            <Typography color="white" component="div" variant="h5" m="8px">
+                                Connect Us
+                            </Typography>
+                        </Box>
+                        <Typography variant="subtitle1" color="white" component="div">
+                            <b>Dominic Networks</b> Pvt. Ltd.
                         </Typography>
                         <Typography variant="subtitle1" color="white" component="div">
-                            Mac Miller
+                            <b>Head Office : </b> A-36, Sector-63 Noida Uttar Pradesh
                         </Typography>
                         <Typography variant="subtitle1" color="white" component="div">
-                            Mac Miller
+                            <b>Call Us: +91-1111111111</b>
                         </Typography>
-                        <Typography variant="subtitle1" color="white" component="div">
-                            Mac Miller
-                        </Typography>
-                        <Typography variant="subtitle1" color="white" component="div">
-                            Mac Miller
-                        </Typography>
-                    </CardContent>   
+                    </CardContent>
                 </Box>
             </Card>
         </>
