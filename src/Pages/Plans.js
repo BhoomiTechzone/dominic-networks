2afd7c70153b498bd3d7c01b47ed54dceffa7bdf
Archanaverma4210 <146517082+Archanaverma4210@users.noreply.github.com>
2023-10-04 13:05:11 +0530

    Update Plans.js

--- src/Pages/Plans.js
+++ src/Pages/Plans.js
@@ -8,15 +8,15 @@ const Plans = () => {
         <div className='container-plans'>
             <Box sx={{ display: "flex", alignItems: "center", flexDirection:"column" }}>
                 <Box sx={{ display: "flex", alignItems: "center" }}>
-                    <Box sx={{ backgroundColor: "white", width: "2px", height: "30px" }} ></Box>
-                    <Typography color="white" component="div" variant="h5" m="8px">
-                        Fiber Plans
+                    <Box sx={{ backgroundColor: "#c53122", width: "2px", height: "30px" }} ></Box>
+                    <Typography color="white" component="div" variant="h4" >
+                    Fiber Plans
                     </Typography>
                 </Box>
-                <Typography color="white">
+                <Typography color="#c53122" variant="h5" >
                     Experience lightning-fast internet with
                 </Typography>
-                <Typography color="white">
+                <Typography color="white" component="div" variant="h4">
                     Our Fiber Plans - Choose the right plan for your needs!
                 </Typography>
             </Box>
