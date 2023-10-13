25273423a9bb3072d60323d6430f4b551d42a493
Abhishek Yadav <92436824+Bhi2710@users.noreply.github.com>
2023-10-13 11:50:24 +0530

    Update Ott.js

--- src/Pages/Ott.js
+++ src/Pages/Ott.js
@@ -1,36 +1,9 @@
 import * as React from 'react';
 import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
 import coupleImg from '../image/couple.jpeg';
 import Form from './Form';
 import SecondNavbar from '../Components/SecondNavbar';
 
-const itemData = [
-    {
-        img: 'https://m.media-amazon.com/images/M/MV5BN2RjZDJhYzUtOTQ5Yy00OWM3LWE5OTctM2Y0YWVmNzAzODllXkEyXkFqcGdeQXVyMTA3MDk2NDg2._V1_SX300.jpg',
-        title: 'movie1',
-    },
-    {
-        img: 'https://m.media-amazon.com/images/M/MV5BZDA0OGQxNTItMDZkMC00N2UyLTg3MzMtYTJmNjg3Nzk5MzRiXkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_SX300.jpg',
-        title: 'movie2',
-    },
-    {
-        img: 'https://m.media-amazon.com/images/M/MV5BNmQ3MThkOWEtNTA0NC00YzI2LWIxZjEtZjdlZTVmNzQ2ZGViXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg',
-        title: 'movie3',
-    },
-    {
-        img: 'https://m.media-amazon.com/images/M/MV5BZDA0OGQxNTItMDZkMC00N2UyLTg3MzMtYTJmNjg3Nzk5MzRiXkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_SX300.jpg',
-        title: 'movie4',
-    },
-    {
-        img: 'https://m.media-amazon.com/images/M/MV5BNTcwMjUwYTQtYTRiYy00ZmNmLWFmNDQtYzQwZTExYjMzZTRiXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg',
-        title: 'movie5',
-    },
-    {
-        img: 'https://m.media-amazon.com/images/M/MV5BYjQ0YTdhZDctYzg5YS00NTNkLThiOWYtNDI5OTYzMGRkYWUyXkEyXkFqcGdeQXVyOTIxMzQ0NzU@._V1_SX300.jpg',
-        title: 'movie6',
-    },
-];
 
 
 const Ott = () => {
@@ -61,16 +34,6 @@ const Ott = () => {
                         </div>
                     </div>
                 </div>
-                <Box sx={{ backgroundColor: "#c53122", display: "flex", flexWrap: 'wrap', justifyContent: "center", gap: "15px", padding: "3rem 0px", cursor: "pointer" }}>
-                    {itemData.map((ele, key) => {
-                        return (
-                            <div key={key} style={{ padding: "" }}>
-                                <img src={ele.img} alt={ele.title} width="200px" height="300px" />
-                            </div>
-                        )
-                    })}
-                </Box>
-
             </div>
             <Form />
         </div>
