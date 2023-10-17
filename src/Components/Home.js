a6448019124cf493f0a4f16e209ac3a98edc723b
Abhishek Yadav <92436824+Bhi2710@users.noreply.github.com>
2023-10-18 00:16:35 +0530

    feedback

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,15 +6,20 @@ import Streaming from '../Pages/Streaming';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import '../Pages/PlanCard.css';
-import OwlCarousel from 'react-owl-carousel';
 import Aos from "aos";
 import 'aos/dist/aos.css';
-import { useEffect } from 'react';
-import 'owl.carousel/dist/assets/owl.carousel.css';
-import 'owl.carousel/dist/assets/owl.theme.default.css';
+import { useEffect,  } from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Rating from '@mui/material/Rating';
+import happy1 from '../image/HappyEmoji.jpg'
 
 
 const Home = () => {
+
+  const value = 5;
 
   useEffect(() => {
     Aos.init({ duration: 2000 });
@@ -31,91 +36,108 @@ const Home = () => {
       <div>
         <Box data-aos="fade-down" sx={{ display: "flex", alignItems: "center", flexDirection: "column", m: "2rem 0px 1rem" }}>
           <Typography color="" variant='h4' textAlign="center">
+            Customer Testimonials
+          </Typography>
+          <Typography color="" variant='h5' textAlign="center">
             What Our Customers are Saying
           </Typography>
         </Box>
-        <div style={{ marginBottom: "0rem" }} >
-          <OwlCarousel
-            className="owl-theme"
-            loop
-            nav
-            margin={8}
-            center
-            dots={false}
-            autoplay
-            responsive={
-              {
-                '1': {
-                  items: 1
-                },
-                '1025': {
-                  items: 3
-                }
-              }
 
-            }
-          >
-            <div style={{ boxShadow: "0px 5px 15px rgba(0, 0, 0, 0.35)", margin: "0px 40px" }} className=""  >
-              <Box sx={{ borderRadius: "10px", padding: "3rem", height: "240px" }}>
-                <Typography sx={{ fontSize: "17px", textAlign: "center" }} color="text.secondary">
-                  I've never experienced a single outage with Dominic Networks' s Fiber Internet and I can't thank them enough.
-                </Typography>
-                <Typography variant='h6' sx={{ textAlign: "center" }} >
-                  Naveen Kumar Gaur
-                </Typography>
-                <Typography sx={{ fontSize: "19px", textAlign: "center" }} color="text.secondary">
-                  Happy Customer
-                </Typography>
-              </Box>
-            </div>
-            
-            <div style={{ boxShadow: "0px 5px 15px rgba(0, 0, 0, 0.35)", margin: "0px 40px" }} className="">
-              <Box sx={{ borderRadius: "10px", padding: "2rem" , height: "240px"}}>
-                <Typography sx={{ fontSize: "17px", textAlign: "center" }} color="text.secondary">
-                  Best Internet Fiber service in town, look no further than Dominic Networks. They've set the bar high for speed and reliability.
-                </Typography>
-                <Typography variant='h6' sx={{ textAlign: "center" }} >
-                  Deepak Singh
-                </Typography>
-                <Typography sx={{ fontSize: "19px", textAlign: "center" }} color="text.secondary">
-                  Happy Customer
-                </Typography>
-              </Box>
-            </div>
 
-            <div style={{ boxShadow: "0px 5px 15px rgba(0, 0, 0, 0.35)", margin: "0px 40px" }} className="">
-              <Box sx={{ borderRadius: "10px", padding: "3rem", height: "240px" }}>
-                <Typography sx={{ fontSize: "17px", textAlign: "center" }} color="text.secondary">
-                  Awesome customer support, they fixed any issue with in given time
-                </Typography>
-                <Typography variant='h6' sx={{ textAlign: "center" }} >
-                  Ankita
-                </Typography>
-                <Typography sx={{ fontSize: "19px", textAlign: "center" }} color="text.secondary">
-                  Happy Customer
-                </Typography>
-              </Box>
-            </div>
+        <div style={{ margin: "2rem", display:'flex',flexWrap: "wrap", gap:"25px", overflow:"scroll"}} >
+          <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+              sx={{ height: 140 }}
+              image={happy1}
+              title="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Lizard
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lizards are a widespread group of squamate reptiles, with over 6,000
+                species, ranging across all continents except Antarctica species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Rating name="read-only" value={value} readOnly />
+            </CardActions>
+          </Card>
+          
+          <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+              sx={{ height: 140 }}
+              image={happy1}
+              title="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Lizard
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lizards are a widespread group of squamate reptiles, with over 6,000
+                species, ranging across all continents except Antarctica species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Rating name="read-only" value={value} readOnly />
+            </CardActions>
+          </Card>
 
-            <div style={{ boxShadow: "0px 5px 15px rgba(0, 0, 0, 0.35)", margin: "0px 40px" }} className="">
-              <Box sx={{ borderRadius: "10px", padding: "3rem" , height: "240px"}}>
-                <Typography sx={{ fontSize: "17px", textAlign: "center" }} color="text.secondary">
-                  Awesome customer support, they fixed any issue with in given time
-                </Typography>
-                <Typography variant='h6' sx={{ textAlign: "center" }} >
-                  Adnna
-                </Typography>
-                <Typography sx={{ fontSize: "19px", textAlign: "center" }} color="text.secondary">
-                  Happy Customer
-                </Typography>
-              </Box>
-            </div>
-          </OwlCarousel>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+              sx={{ height: 140 }}
+              image={happy1}
+              title="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Lizard
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lizards are a widespread group of squamate reptiles, with over 6,000
+                species, ranging across all continents except Antarctica species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Rating name="read-only" value={value} readOnly />
+            </CardActions>
+          </Card>
+          
+          <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+              sx={{ height: 140 }}
+              image={happy1}
+              title="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Lizard
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lizards are a widespread group of squamate reptiles, with over 6,000
+                species, ranging across all continents except Antarctica species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Rating name="read-only" value={value} readOnly />
+            </CardActions>
+          </Card>
         </div>
-
       </div>
     </>
   )
 }
 
-export default Home
+export default Home;
+
+
+
+
+
+
+
+
+
+
