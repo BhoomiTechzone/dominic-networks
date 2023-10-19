e899d6c8f7b882baac986db4cfdffc12accf1a40
Abhishek Yadav <92436824+Bhi2710@users.noreply.github.com>
2023-10-19 20:07:03 +0530

    19)ct

--- src/Pages/Iptv.js
+++ src/Pages/Iptv.js
@@ -18,20 +18,20 @@ function Iptv(props) {
 
         <div className="container iptv-content">
           <div className="row">
-            <div className="col-lg-7">
+            <div className="col-lg-6">
               <div className="iptv-content-left">
-                <h2>Internet Lease Line (iptv)</h2> 
+                <h2>Internet Lease Line (iptv)</h2>
                 <p>
                   IPTV, short for Internet Protocol Television, is a cutting-edge technology that revolutionizes the way television and multimedia content is delivered and consumed. Unlike traditional cable or satellite TV, IPTV relies on internet protocols to transmit a wide array of content, including live TV channels, video-on-demand, and interactive services. Users can access IPTV on various devices such as smart TVs, computers, smartphones, and dedicated set-top boxes. To enjoy IPTV, individuals typically need a subscription and a stable high-speed internet connection. This technology offers an array of features like time-shifting, allowing users to pause, rewind, or record live TV, and extensive video-on-demand libraries for on-demand viewing. IPTV is known for its interactive capabilities, with on-screen menus, program guides, and content recommendations. It's widely used in diverse settings, including hotels, businesses, and residential areas, offering customized and high-quality digital content delivery in both standard-definition (SD) and high-definition (HD) formats. IPTV's efficient content distribution methods make it an increasingly popular choice, eliminating the need for traditional cable or satellite infrastructure while providing personalized and flexible content consumption experiences for users.</p>
               </div>
             </div>
-            <div className="col-lg-5">
+            <div className="col-lg-6">
               <div className="iptv-content-left-img">
-                <img src={iptv1} alt="iptv" height="240px" width="240px"  />
-                <img src={iptv3} alt="iptv" height="240px" width="240px" />
-              </div>
-              <div className='iptv-content-left-imageLast'>
-                <img src={iptv4} alt="iptv" height="240px" width="240px" />
+                <img src={iptv1} alt="iptv" height="240px" width="280px" />
+                <img src={iptv3} alt="iptv" height="240px" width="280px" />
+                <div className='iptv-content-left-imageLast'>
+                  <img src={iptv4} alt="iptv" height="240px" width="300px" />
+                </div>
               </div>
             </div>
           </div>
